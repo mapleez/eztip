71d865cef89842d515aad1bd957a63caf983e55e
mapleez <kukunanhai_5207@126.com>
2016-10-08 10:50:25 +0800

    calling point class.

--- scala/hello.scala
+++ scala/hello.scala
@@ -1,6 +1,10 @@
 object hello {
 	def main (args : Array [String]) : Unit = {
-		println ("Hello, World!");
+		// println ("Hello, World!");
+		val pt = new point (23, 32)
+		pt.disp ()
+		pt.move (32, -3)
+		pt.disp ()
 	}
 }
 

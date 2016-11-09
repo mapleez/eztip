4dcab4293877e2fd18dc5c762ee496e821320dc3
mapleez <kukunanhai_5207@126.com>
2016-11-09 17:29:59 +0800

    Use a list instead of a FilterList instance to store filter before building Scan.

--- scala/src/dt/ez/util/hbase/ScanUtils.scala
+++ scala/src/dt/ez/util/hbase/ScanUtils.scala
@@ -14,11 +14,20 @@ import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp
 class ScanUtils extends Tools {
   
   val scan = new Scan ()
-  val filterList = new FilterList ()
+  
+  /** If you use a FilterList instance. Then you'd
+   * better ensure the instance has at list one 
+   * filter underlying. Or an unknown result will 
+   * occurred while scan data after invoking scan.setFilter.
+   * 
+   * So I use a List to store filters and invoking setFilter
+   * in method get().
+   */
+  val filters = List [Filter] ()
   
   def limit (num: Long) : ScanUtils = {
     scan.setCaching (1)
-    filterList.addFilter (new PageFilter (num))
+    filters :+ new PageFilter (num)
     this
   }
   
@@ -30,14 +39,14 @@ class ScanUtils extends Tools {
   def keySmallerThan (key: String, equal: Boolean = true) : ScanUtils = {
     val cmp = if (equal) CompareOp.LESS_OR_EQUAL else CompareOp.LESS
     val rowkeyFilter = new RowFilter (cmp, new BinaryComparator (key.getBytes))
-    filterList.addFilter (rowkeyFilter)
+    filters :+ rowkeyFilter
     this
   }
   
   def keyBiggerThan (key: String, equal: Boolean = true) : ScanUtils = {
     val cmp = if (equal) CompareOp.LESS_OR_EQUAL else CompareOp.LESS
     val rowkeyFilter = new RowFilter (cmp, new BinaryComparator (key))
-    filterList.addFilter (rowkeyFilter)
+    filters :+ rowkeyFilter
     this
   }
   
@@ -63,13 +72,17 @@ class ScanUtils extends Tools {
     val cmpSta = if (eqSta) CompareOp.GREATER_OR_EQUAL else CompareOp.GREATER
     val cmpEnd = if (eqEnd) CompareOp.LESS_OR_EQUAL else CompareOp.LESS
     /* Set Start key, end key. */
-    filterList.addFilter (new RowFilter (cmpSta, new BinaryComparator (start)))
-    filterList.addFilter (new RowFilter (cmpEnd, new BinaryComparator (end)))
+    filters :+ new RowFilter (cmpSta, new BinaryComparator (start))
+    filters :+ new RowFilter (cmpEnd, new BinaryComparator (end))
     this
   }
   
   def get : Scan = {
-    this.scan.setFilter (filterList)
+    if (! filters.isEmpty) {
+      val filterList = new FilterList ()
+      filters.foreach { filterList.addFilter (_) }
+      this.scan.setFilter (filterList)
+    }
     this.scan
   }
   
